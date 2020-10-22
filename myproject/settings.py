28bc94ede6491f88fd6d319351aca1584351741d
Ubuntu <ubuntu@ip-172-31-6-22.us-west-2.compute.internal>
2020-10-22 17:32:05 +0000

    setup still

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = base_config.get('General', 'secret_key')
-print(SECRET_KEY)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
